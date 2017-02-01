6d30b0a1d4a0a020a0571bfa43c5deb7787ab900
Guillaume MOREL <g.morel@imediafrance.fr>
2017-02-01 21:54:26 +0100

    Add code coverage on Atoum unit tests

--- .atoum.php
+++ .atoum.php
@@ -7,4 +7,20 @@ $report = $script->addDefaultReport();
 // This will add a green or red logo after each run depending on its status.
 $report->addField(new atoum\report\fields\runner\result\logo());
 
+/*
+This will ad the default CLI report
+*/
+$script->addDefaultReport();
+
+$cloverWriter = new atoum\writers\file('atoum.coverage.xml');
+
+// Generate a clover XML report.
+$cloverReport = new atoum\reports\asynchronous\clover();
+$cloverReport->addWriter($cloverWriter);
+
+$runner->addReport($cloverReport);
+
 $runner->addTestsFromDirectory(__DIR__.'/tests/units');
+
+
+
