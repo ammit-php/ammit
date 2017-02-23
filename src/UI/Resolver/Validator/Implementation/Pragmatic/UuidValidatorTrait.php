9854fadc05086fee4ea1899d62ca56bdd3825615
Guillaume MOREL <g.morel@imediafrance.fr>
2017-02-23 22:25:50 +0100

    Extract UUID validator into dedicated Trait
    In order to ease Unit Test redability

--- src/UI/Resolver/Validator/Implementation/Pragmatic/UuidValidatorTrait.php
+++ src/UI/Resolver/Validator/Implementation/Pragmatic/UuidValidatorTrait.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Imedia\Ammit\UI\Resolver\Validator\Implementation\Pure;
+namespace Imedia\Ammit\UI\Resolver\Validator\Implementation\Pragmatic;
 
 use Assert\Assertion;
 use Imedia\Ammit\UI\Resolver\Validator\UIValidatorInterface;
@@ -8,30 +8,31 @@ use Imedia\Ammit\UI\Resolver\Validator\UIValidatorInterface;
 /**
  * @author Guillaume MOREL <g.morel@imediafrance.fr>
  */
-trait StringValidatorTrait
+trait UuidValidatorTrait
 {
     /**
+     * Domain should be responsible for id format
      * Exceptions are caught in order to be processed later
-     * @param mixed $value String ?
+     * @param mixed $value UUID ?
      *
      * @return string Casted to string
      */
-    public function mustBeString($value, string $propertyPath = null, UIValidatorInterface $parentValidator = null, string $exceptionMessage = null): string
+    public function mustBeUuid($value, string $propertyPath = null, UIValidatorInterface $parentValidator = null, string $exceptionMessage = null): string
     {
+        if (null === $value || !is_string($value)) {
+            $value = '';
+        }
+
         $this->validationEngine->validateFieldValue(
             $parentValidator ?: $this,
             function() use ($value, $propertyPath, $exceptionMessage) {
-                Assertion::string(
+                Assertion::uuid(
                     $value,
                     $exceptionMessage,
                     $propertyPath
                 );
             }
         );
-
-        if (null === $value || !is_string($value)) {
-            return '';
-        }
 
         return (string) $value;
     }
