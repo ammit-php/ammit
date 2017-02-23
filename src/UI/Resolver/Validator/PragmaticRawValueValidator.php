5958d185d2a596fe909d0f3c1cf19ea132e0316d
Guillaume MOREL <g.morel@imediafrance.fr>
2017-02-23 22:25:50 +0100

    Extract UUID validator into dedicated Trait
    In order to ease Unit Test redability

--- src/UI/Resolver/Validator/PragmaticRawValueValidator.php
+++ src/UI/Resolver/Validator/PragmaticRawValueValidator.php
@@ -6,6 +6,7 @@ namespace Imedia\Ammit\UI\Resolver\Validator;
 use Assert\Assertion;
 use Assert\InvalidArgumentException;
 use Imedia\Ammit\Domain\MailMxValidation;
+use Imedia\Ammit\UI\Resolver\Validator\Implementation\Pragmatic\UuidValidatorTrait;
 
 /**
  * @internal Contains Domain Validation assertions (but class won't be removed in next version)
@@ -16,28 +17,7 @@ use Imedia\Ammit\Domain\MailMxValidation;
  */
 class PragmaticRawValueValidator extends RawValueValidator
 {
-    /**
-     * Domain should be responsible for id format
-     * Exceptions are caught in order to be processed later
-     * @param mixed $value String ?
-     *
-     * @return mixed Untouched value
-     */
-    public function mustBeUuid($value, string $propertyPath = null, UIValidatorInterface $parentValidator = null, string $exceptionMessage = null)
-    {
-        $this->validationEngine->validateFieldValue(
-            $parentValidator ?: $this,
-            function() use ($value, $propertyPath, $exceptionMessage) {
-                Assertion::uuid(
-                    $value,
-                    $exceptionMessage,
-                    $propertyPath
-                );
-            }
-        );
-
-        return $value;
-    }
+    use UuidValidatorTrait;
 
     /**
      * Domain should be responsible for id format
